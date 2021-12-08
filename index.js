c5a500abe191723f05899778dc485f867e43471b
neohacker18 <46134586+neohacker18@users.noreply.github.com>
2021-12-08 22:54:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+// var myVar = setInterval(setColor, 3000);
+// var cnt=0; 
+// var role='Developer'
+// var x = document.getElementById("developer");
+// function setColor() {
+//     cnt++;
+//   if(cnt%2==0)
+//   {
+//       role='A Developer.'
+//     var op = 0.1;
+//     x.style.display = 'block';
+//     var timer = setInterval(function () {
+//         if (op >= 1){
+//             clearInterval(timer);
+//         }
+//         x.style.opacity = op;
+//         x.style.filter = 'alpha(opacity=' + op * 100 + ")";
+//         op += op * 0.1;
+//     }, 20);
+//       x.innerHTML=`${role}`
+//   }
+//   else
+//   {
+//       role='A Designer.'
+//     var op = 1;  
+//     var timer = setInterval(function () {
+//         if (op <= 0.1){
+//             clearInterval(timer);
+//             x.style.display = 'none';
+//         }
+//         x.style.opacity = op;
+//         x.style.filter = 'alpha(opacity=' + op * 100 + ")";
+//         op -= op * 0.1;
+//     }, 50);
+//       x.innerHTML=`${role}`
+//   }
+// }
